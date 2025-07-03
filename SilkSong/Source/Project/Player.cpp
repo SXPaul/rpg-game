7dd1d860264e9c0244f786a80705e3af391e88cf
djwwwww <djw20050103@163.com>
2025-07-03 16:17:03 +0800

    Updated the jump logic

--- SilkSong/Source/Project/Player.cpp
+++ SilkSong/Source/Project/Player.cpp
@@ -4,6 +4,7 @@
 #include "Components/RigidBody.h"
 #include "Components/Camera.h"
 #include "PlayerAnimator.h"
+#include "GameplayStatics.h"
 #include <cstdlib>
 Player::Player()
 {
@@ -30,7 +31,14 @@ Player::Player()
         rigid->SetGravity(1960);
     }
 
+
+    box->OnComponentHit.AddDynamic(this, &Player::StartCollision);
+    box->OnComponentStay.AddDynamic(this, &Player::StayCollision);
+
     walkLock = 0;
+	jumpLock = 0;
+    lastJumpTime = 0.0f; // 初始化上次跳跃时间戳
+	isonGround = false;
 }
 
 void Player::BeginPlay()
@@ -53,6 +61,11 @@ void Player::SetupInputComponent(InputComponent* inputComponent)
     inputComponent->SetMapping("WalkRight", EKeyCode::VK_D);
     inputComponent->SetMapping("WalkLeftEnd", EKeyCode::VK_A);
     inputComponent->SetMapping("WalkRightEnd", EKeyCode::VK_D);
+	inputComponent->SetMapping("JumpStart", EKeyCode::VK_Space);
+	inputComponent->SetMapping("Jumping", EKeyCode::VK_Space);
+	inputComponent->SetMapping("JumpEnd", EKeyCode::VK_Space);
+
+
 
     inputComponent->BindAction("WalkLeft", EInputType::Holding, [this]() {
         if (walkLock == 2)
@@ -85,6 +98,52 @@ void Player::SetupInputComponent(InputComponent* inputComponent)
         ani->SetFloat("walkingSpeed", 0.f);
 
         });
+
+    inputComponent->BindAction("JumpStart", EInputType::Pressed, [this]()
+        {
+            if (isonGround)
+            {
+                // 如果玩家在地面上
+                rigid->SetVelocity(FVector2D(rigid->GetVelocity().x, -400.f)); // 向上跳跃
+                isonGround = false; // 跳跃后标记不在地面上
+                // 切换到跳跃动画
+                //
+                //
+                //
+                lastJumpTime = GameplayStatics::GetTimeSeconds(); // 记录跳跃时间
+                jumpLock = 1; // 设置跳跃锁，防止连续跳跃,jumpLock = 1 代表已经开始跳跃，但是还没结束跳跃
+            }
+			else return; // 如果不在地面上，则不允许跳跃
+        });
+
+    inputComponent->BindAction("Jumping", EInputType::Holding, [this]()
+        {
+            if (jumpLock == 1 && GameplayStatics::GetTimeSeconds() - lastJumpTime < 0.5f)
+            {
+				isonGround = false; // 标记玩家不在地面上
+                // 如果在跳跃状态且跳跃时间小于0.2秒
+				rigid->SetVelocity(FVector2D(rigid->GetVelocity().x, -400.f)); // 持续给一个向上的速度
+                // 跳跃动画
+                //
+                //
+                //
+            }
+		});
+
+    inputComponent->BindAction("JumpEnd", EInputType::Released, [this]()
+        {
+            if (jumpLock == 1)
+            {
+                // 如果跳跃结束
+                jumpLock = 0; // 重置跳跃锁
+                isonGround = false; // 标记玩家不在地面上
+                rigid->SetVelocity(FVector2D(rigid->GetVelocity().x, 0)); // 重置垂直速度
+                // 切换到站立动画
+                //
+                //
+                //
+            }
+		});
 }
 
 FVector2D Player::GetCameraPos()
@@ -92,3 +151,34 @@ FVector2D Player::GetCameraPos()
     if (camera)return camera->GetVirtualPosition();
     return FVector2D();
 }
+
+void Player::StartCollision(Collider* hitComp, Collider* otherComp, Actor* otherActor, FVector2D normalImpulse, const FHitResult& hitResult)
+{
+	//处理碰撞开始的逻辑
+    if (normalImpulse.y < 0 && rigid && rigid->GetVelocity().y > 0)
+    {
+        // 如果碰撞是从上方发生的，并且玩家正在向下移动
+		isonGround = true; // 标记玩家在地面上
+        rigid->SetVelocity(FVector2D(rigid->GetVelocity().x, 0)); // 重置垂直速度
+        //此处插入站立动画
+
+    }
+    else if (normalImpulse.y > 0 && rigid && rigid->GetVelocity().y < 0)
+    {
+        // 如果碰撞是从下方发生的，并且玩家正在向上移动
+		isonGround = false; // 标记玩家不在地面上
+        rigid->SetVelocity(FVector2D(rigid->GetVelocity().x, 0)); // 重置垂直速度
+	}
+    
+}
+
+void Player::StayCollision(Collider* hitComp, Collider* otherComp, Actor* otherActor, FVector2D normalImpulse, const FHitResult& hitResult)
+{
+    // 在这里处理持续碰撞的逻辑
+    // 例如，可以检查碰撞的类型并执行相应的操作
+    if (normalImpulse.y < 0)
+    {
+        isonGround = true; // 标记玩家在地面上
+    }
+}
+
--- SilkSong/Source/Project/Player.h
+++ SilkSong/Source/Project/Player.h
@@ -13,6 +13,9 @@ public:
 
 protected:
     virtual void SetupInputComponent(InputComponent* inputComponent) override;
+    void StartCollision(Collider* hitComp, Collider* otherComp, Actor* otherActor, FVector2D normalImpulse, const FHitResult& hitResult);
+    void StayCollision(Collider* hitComp, Collider* otherComp, Actor* otherActor, FVector2D normalImpulse, const FHitResult& hitResult);
+
 
 private:
     class SpriteRenderer* render;
@@ -21,5 +24,9 @@ private:
     class Camera* camera;
     class PlayerAnimator* ani;
 
-    int32 walkLock;
+	bool isonGround;
+
+    int32 walkLock = 0;
+    int32 jumpLock = 0;
+	float lastJumpTime = 0.0f; // 上次跳跃的时间戳
 };
